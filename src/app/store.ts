eb7c298c736e43720ca8a3a5a32a1743f8022d7e
MD Nelles <devstudioca@gmail.com>
2022-09-10 14:18:34 -0500

    forecast reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import currentReducer from "../features/current/currentSlice";
+import forecastReducer from "../features/forecast/forecastSlice";
 
 export const store = configureStore({
    reducer: {
       counter: counterReducer,
       current: currentReducer,
+      forecast: forecastReducer,
    },
 });
 
