6c0406d8cc251ea9c54f1b64eb4ca7b55d01a0e9
MD Nelles <devstudioca@gmail.com>
2022-09-15 14:31:42 -0500

    add config

---
+++ src/utils/config.ts
@@ -0,0 +1,3 @@
+export const API_KEY = "506e068052284ac91fecc891905ede01";
+export const API_URL = "http://api.openweathermap.org/data/2.5/forecast";
+export const WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
